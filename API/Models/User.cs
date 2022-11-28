55c23e7e38aac0738c19610a88e7157e81b14d1b
Durga Prasad <durgaprasad.thoutam@gmail.com>
2022-11-28 12:50:28 +0000

    Committing changes

---
+++ API/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Sockets;
+
+namespace API.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public Address Address { get; set; }
+}
---
+++ API/Services/UserService.cs
@@ -0,0 +1,21 @@
+﻿using API.Models;
+
+namespace API.Services;
+
+public interface IUserService
+{
+    public Task<List<User>> GetAllUsers();
+}
+public class UserService:IUserService
+{
+    public UserService()
+    {
+        
+    }
+
+
+    public Task<List<User>> GetAllUsers()
+    {
+        throw new NotImplementedException();
+    }
+}
